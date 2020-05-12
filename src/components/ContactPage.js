e74e5323253d6a620d1a66e903750a2afef93279
Lyly <lyly2112@gmail.com>
2020-05-11 23:24:56 -0700

    added content to Contact Page

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -5,7 +5,24 @@ function ContactPage(){
         <div className="contact-page-container">
             
             {/* <NavbarServices/> */}
-            <div className="contact-page__main">contact page</div>
+            <div className="contact-page__main">
+                <div className="contact-form">
+                    <div className="contact-form__name">
+                    <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+                    </div>
+                    <div className="contact-form__email">Your email</div>
+                    <div className="contact-form__phone">Your phone</div>
+                    <div className="contact-form__message">Type your message here</div>
+                    <div className="contact-button">Send</div>
+                </div>
+            <div className="contact-page_my-contacts">Seattle, WA</div>
+            </div>
             {/* <Footer/> */}
         </div>
     )
