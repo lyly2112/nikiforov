f71bd24228c23ecb52023e0cef3b9a57bb8d4bf4
Sergey <in43sh@gmail.com>
2020-05-22 13:15:46 -0600

    added box-sizing

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -16,7 +16,6 @@ function AboutPage(){
                 </div>
                 <WorkWithMe/>
             </div>
-            {/* <Footer/> */}
         </div>
     )
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,20 +7,17 @@ function HomePage() {
     <div className="home-page-container">
       <div className="main-container>">
       <div className="home-page__main-photo">
-        <img src={require('../images/home-page-photo.jpg')} alt="mountain"/> 
-      </div>
         <div className="home-page__intro-container">
             <p>Sergei Nikiforov</p>
             <p>freelance photographer</p>
-            <p>I am Sergei Nikiforov, a freelance photographer 
-            specializing in landscape, advertorial and conceptual
-            photography, based in Seattle. Here you will be able 
-            to find my latest and favorite shots.</p>
+            
         </div>
+      
+      </div>
+        
         <Gallery/>
         <WorkWithMe/>
       </div>
-      {/* <Footer/> */}
     </div>
     )
 }
