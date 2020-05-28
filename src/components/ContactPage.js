c3b4df9d597beaf68dfd686587c6b66bd89c7183
Lyly <lyly2112@gmail.com>
2020-05-28 01:49:20 -0700

    added pdding to contact form placeholder

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -13,13 +13,9 @@ function ContactPage(){
               <div className="main-container__column">
                 <div className="contact-form">
                   <form className="contact-form__form">
-                    {/* <label for="contact-form__name" className="contact-form__input-area">Name</label> */}
                     <input className="contact-form__input" type="text" name="name" placeholder="Your name" />
-                    {/* <label for="contact-form__email" className="contact-form__input-area">Email</label> */}
                     <input className="contact-form__input" type="email" name="email" placeholder="Your email" />
-                    {/* <label for="contact-form__phone" className="contact-form__input-area">Phone</label> */}
                     <input className="contact-form__input" type="tel" name="phone" placeholder="Your phone number" />
-                    {/* <label for="contact-form__message" className="contact-form__input-area">Message</label> */}
                     <textarea className="contact-form__message" rows="6" type="message" name="comments" placeholder="Type your message here" />
                   </form>
                   <div className="send-button">Send</div>
