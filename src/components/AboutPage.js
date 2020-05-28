b0a93a53d5fe96cc0bc30eb047928053e4807dd9
Lyly <lyly2112@gmail.com>
2020-05-28 14:05:37 -0700

    fixed footer bug

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -7,7 +7,7 @@ function AboutPage(){
             <div className="main-container">
                 <div className="main-container__header">Who is behind?</div>
                 <div className="bio-container">
-                        <img className="photo" src={avatar} alt="Avatar" />
+                        <img className="avatar" src={avatar} alt="Avatar" />
                     <div className="bio">
                         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Nullam id dolor id nibh ultricies vehicula ut id elit, fusce dapibus, tellus ac cursus commodo. Cras mattis consectetur purus sit amet fermentum. Vestibulum id ligula.</p>
                     </div>
