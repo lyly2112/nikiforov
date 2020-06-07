83f926da8188222fa0695ce0800c99568d7a882d
Lyly <lyly2112@gmail.com>
2020-06-07 01:05:03 -0700

    changed mobile versoin to one-page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import NavbarMenu from './components/NavbarMenu';
 import Footer from './components/Footer';
 import routes from './routes';
+import Home from './components/pages/HomePage';
+import About from './components/pages/AboutPage';
+import Services from './components/pages/ServicesPage';
+import ContactUs from './components/pages/ContactPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavbarMenu/>
-        { routes }
+        <div className="mobile">
+          <Home/>
+          <Services/>
+          <About/>
+          <ContactUs/>
+        </div>
+        <div classNAme="desktop">
+         { routes }
+        </div>
         <Footer/>
       </div>
     );
