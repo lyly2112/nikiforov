df54923da1e7e007980a2f9a601c9d0ea772e491
lyly2112 <lyly2112@gmail.com>
2021-10-21 21:38:42 -0700

    Add link for youtube to Footer component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import instagram from '../images/instagram.svg';
-import artpal from '../images/artpal.png';
+// import artpal from '../images/artpal.png';
+import youtube from '../images/youtube.svg';
 import { ExternalLink } from 'react-external-link';
 
 function Footer() {
@@ -14,9 +15,12 @@ function Footer() {
                     <ExternalLink href="https://www.instagram.com/serjnike/">
                         <img className="social-media--footer" src={instagram} alt="instagram"></img>
                     </ExternalLink>
-                    <ExternalLink href="https://www.artpal.com/Sergeinikiforoff">
-                        <img className="social-media--footer" src={artpal} alt="artpal"></img>
+                    <ExternalLink href="https://www.youtube.com/channel/UCIc6I8dbsjwYC3-GjZly1Hg">
+                        <img className="social-media--footer" src={youtube} alt="youtube"></img>
                     </ExternalLink>
+                    {/* <ExternalLink href="https://www.artpal.com/Sergeinikiforoff">
+                        <img className="social-media--footer" src={artpal} alt="artpal"></img>
+                    </ExternalLink> */}
             </div>
             <span className="footer__signature__column">Site by
                 <span className="footer__bold"> Majestic Bay</span>
